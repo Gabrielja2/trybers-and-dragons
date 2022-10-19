c2b2c367055d3c455b0becadacfcdfb19d4bb53b
Gabriel <gabrielja2antunes@gmail.com>
2022-10-19 16:43:06 -0300

    feat: criado class SimpleFighter req.09 finalizado

---
+++ src/Monster.ts
@@ -0,0 +1,28 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+
+  private _strength: number;
+
+  constructor() {
+    this._lifePoints = 85;
+    this._strength = 63;
+  }
+
+  get lifePoints() { return this._lifePoints; }
+
+  get strength() { return this._strength; }
+
+  receiveDamage(attackPoints: number): number {
+    const damage = this.lifePoints - attackPoints;
+    if (attackPoints > 0) return damage;
+    if (this._lifePoints <= 0) this._lifePoints = -1;
+
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
